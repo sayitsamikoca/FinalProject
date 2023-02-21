741ad504270f1bae62e57c87d8a977a017a462e1
Sayit Sami Koca <sayitsamikoca0@gmail.com>
2023-02-21 23:58:14 +0300

    code refactoring

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,11 +1,12 @@
-﻿using Entities.Concrete;
+﻿using Core.DataAcces;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDal:IEntityRepository<Product>
+    public interface IProductDal : IEntityRepository<Product>
     {
        
     }
