59c8b55a00a5c67e2327f08b5e51d6135810df98
Sayit Sami Koca <sayitsamikoca0@gmail.com>
2023-03-06 19:57:18 +0300

    Yeni standart mesajlar eklendi(field added)

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,5 +10,7 @@ namespace Business.Constants
         public static string ProductAdded = "Ürün Eklendi.";
 
         public static string ProductNameInvalid = "Ürün ismi geçersiz.";
+        internal static List<Product> MaintenanceTime;
+        internal static string ProductsListed;
     }
 }
