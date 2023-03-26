7fedafcc1efc80e61f2b683ac5704742df2a0c76
Sayit Sami Koca <sayitsamikoca0@gmail.com>
2023-03-26 23:57:59 +0300

    11. gün ileri seviye web api kodlama dersi tamamlandı.

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -22,11 +22,41 @@ namespace WebAPI.Controllers
             _productService = productService;
         }
 
-        [HttpGet]
-        public List<Product> Get()
+        [HttpGet("getall")]
+        public IActionResult GetAll()
         {
+            // swagger
+            // dependency chain 
+
             var result = _productService.GetAll();
-            return result.Data;
+            if (result.Succes)
+            {
+                return Ok(result); 
+            }
+            return BadRequest(result);    
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Succes)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Succes)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
         }
 
     }
